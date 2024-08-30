cde993293b4afc5a59399d3dd479a38cfd6a1172
Patryk Pastuszka <nov11157@gmail.com>
2024-08-30 21:40:21 +0200

    Singleton tests

---
+++ Assets/FastUnityCreationKit/Tests/Core/Utility/Data/RegularClassSingleton.cs
@@ -0,0 +1,12 @@
+﻿using FastUnityCreationKit.Core.Utility.Singleton;
+
+namespace FastUnityCreationKit.Tests.Core.Utility.Data
+{
+    /// <summary>
+    /// Test class for singleton.
+    /// </summary>
+    public sealed class RegularClassSingleton : ISingleton<RegularClassSingleton>
+    {
+        
+    }
+}
---
+++ Assets/FastUnityCreationKit/Tests/Core/Utility/SingletonTests.cs
@@ -0,0 +1,84 @@
+﻿using FastUnityCreationKit.Core.Testing;
+using FastUnityCreationKit.Core.Utility.Singleton;
+using FastUnityCreationKit.Tests.Core.Utility.Data;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace FastUnityCreationKit.Tests.Core.Utility
+{
+    [TestFixture]
+    public class SingletonTests
+    {
+
+        [Test]
+        public void GetInstance_CreatesInstanceOfCSharpClass_IfSingletonInstance_IsNull()
+        {
+            // Act
+            RegularClassSingleton singleton = ISingleton<RegularClassSingleton>.GetInstance();
+            
+            // Assert
+            Assert.IsNotNull(singleton);
+        }
+        
+        [Test]
+        public void GetInstance_CreatesInstanceOfMonoBehaviourClass_IfSingletonInstance_IsNull()
+        {
+            // Act
+            ExampleMonoBehaviourClassSingleton singleton = IMonoBehaviourSingleton<ExampleMonoBehaviourClassSingleton>.GetInstance();
+            
+            // Assert
+            Assert.IsNotNull(singleton);
+            
+            // Cleanup
+            Object.DestroyImmediate(singleton.gameObject);
+        }
+        
+        [Test]
+        public void GetInstance_ReturnsSameInstanceOfCSharpClass_IfSingletonInstance_IsNotNull()
+        {
+            // Arrange
+            RegularClassSingleton singleton = ISingleton<RegularClassSingleton>.GetInstance();
+            
+            // Act
+            RegularClassSingleton newSingleton = ISingleton<RegularClassSingleton>.GetInstance();
+            
+            // Assert
+            Assert.AreSame(singleton, newSingleton);
+        }
+        
+        [Test]
+        public void GetInstance_ReturnsSameInstanceOfMonoBehaviourClass_IfSingletonInstance_IsNotNull()
+        {
+            // Arrange
+            ExampleMonoBehaviourClassSingleton singleton = IMonoBehaviourSingleton<ExampleMonoBehaviourClassSingleton>.GetInstance();
+            
+            // Act
+            ExampleMonoBehaviourClassSingleton newSingleton = IMonoBehaviourSingleton<ExampleMonoBehaviourClassSingleton>.GetInstance();
+            
+            // Assert
+            Assert.AreSame(singleton, newSingleton);
+            
+            // Cleanup
+            Object.DestroyImmediate(singleton.gameObject);
+        }
+
+        [Test]
+        public void GetInstance_CreatesNewInstanceOfSingleton_IfGameObjectWasDestroyed_InTheMeanwhile()
+        {
+            // Arrange
+            ExampleMonoBehaviourClassSingleton singleton = IMonoBehaviourSingleton<ExampleMonoBehaviourClassSingleton>.GetInstance();
+            Object.DestroyImmediate(singleton.gameObject);
+            
+            // Act
+            ExampleMonoBehaviourClassSingleton newSingleton = IMonoBehaviourSingleton<ExampleMonoBehaviourClassSingleton>.GetInstance();
+            
+            // Assert
+            Assert.IsNotNull(newSingleton);
+            Assert.AreNotSame(singleton, newSingleton);
+            
+            // Cleanup
+            Object.DestroyImmediate(newSingleton.gameObject);
+        }
+        
+    }
+}
