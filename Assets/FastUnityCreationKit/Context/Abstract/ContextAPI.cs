9d16e4d650e0db6359f0aa478570797b82949683
Patryk Pastuszka <nov11157@gmail.com>
2024-11-25 23:12:23 +0100

    Start of ContextAPI HAL

---
+++ Assets/FastUnityCreationKit/Context/Abstract/ContextAPI.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FastUnityCreationKit.Context.Abstract
+{
+    /// <summary>
+    /// Context API is used to provide quick access for common context analysis operations.
+    /// </summary>
+    public static class ContextAPI
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsAnyContext<TContextType>() => typeof(TContextType) == typeof(AnyUsageContext);
+    }
+}
