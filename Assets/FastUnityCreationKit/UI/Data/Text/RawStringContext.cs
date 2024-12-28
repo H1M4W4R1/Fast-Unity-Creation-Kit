e1a008249ec4d851eda07c3b1f3bd48c1e118bf6
Patryk Pastuszka <nov11157@gmail.com>
2024-12-28 01:29:18 +0100

    Added RawStringContext automatic updates when value is changed.

--- Assets/FastUnityCreationKit/UI/Data/Text/RawStringContext.cs
+++ Assets/FastUnityCreationKit/UI/Data/Text/RawStringContext.cs
@@ -1,13 +1,25 @@
-﻿using Sirenix.OdinInspector;
+﻿using FastUnityCreationKit.Unity.Events.Interfaces;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace FastUnityCreationKit.UI.Data.Text
 {
-    public sealed class RawStringContext : StringContextBase<RawStringContext>
+    public sealed class RawStringContext : StringContextBase<RawStringContext>, IUpdateCallback
     {
         [TabGroup("Configuration")] [ShowInInspector] [SerializeField]
         private string text;
+        private string _renderedText;
 
         public override string LocalizedText => text;
+
+        public void OnObjectUpdated(float deltaTime)
+        {
+            // Check if text has changed and update it
+            if (_renderedText == text) return;
+            _renderedText = text;
+            
+            // Notify that the object has been updated
+            MakeDirty();
+        }
     }
 }
