f02157e3a27f4934359785461d26660954b1046c
Patryk Pastuszka <nov11157@gmail.com>
2024-12-28 13:44:36 +0100

    ResourceDatabase get resource by type

--- Assets/FastUnityCreationKit/Economy/ResourceDatabase.cs
+++ Assets/FastUnityCreationKit/Economy/ResourceDatabase.cs
@@ -15,6 +15,11 @@ namespace FastUnityCreationKit.Economy
             addressableTag = LocalConstants.RESOURCE_ADDRESSABLE_TAG;
         }
 
+        /// <summary>
+        /// Get resource by identifier.
+        /// </summary>
+        /// <param name="identifier">Identifier of resource to get.</param>
+        /// <returns>Resource with specified identifier or null if not found.</returns>
         [CanBeNull] public ResourceBase GetResource(Snowflake128 identifier)
         {
             for(int i = 0; i < Count; i++)
@@ -24,6 +29,26 @@ namespace FastUnityCreationKit.Economy
                 
                 if (resource.Id == identifier)
                     return this[i];
+            }
+            
+            return null;
+        }
+        
+        /// <summary>
+        /// Get resource of specified type.
+        /// </summary>
+        /// <typeparam name="TResource">Type of resource to get.</typeparam>
+        /// <returns>Resource of specified type or null if not found.</returns>
+        [CanBeNull] public TResource GetResource<TResource>() 
+            where TResource : ResourceBase
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                ResourceBase resource = this[i];
+                if (!resource) continue;
+                
+                if (resource is TResource castedResource)
+                    return castedResource;
             }
             
             return null;
