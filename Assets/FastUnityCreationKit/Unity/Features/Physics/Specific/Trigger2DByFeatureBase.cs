afd5824f9041199b960ed83041f7d6b774472ea9
Patryk Pastuszka <nov11157@gmail.com>
2025-01-04 02:52:33 +0100

    Trigger Features naming fix to be more descriptive.

--- Assets/FastUnityCreationKit/Unity/Features/Physics/Specific/Trigger2DByFeatureBase.cs
+++ Assets/FastUnityCreationKit/Unity/Features/Physics/Specific/Trigger2DByFeatureBase.cs
@@ -7,32 +7,32 @@ using UnityEngine;
 namespace FastUnityCreationKit.Unity.Features.Physics.Specific
 {
     /// <summary>
-    /// Feature base for 3D triggers
+    /// Feature base for 2D triggers
     /// </summary>
-    public abstract class Trigger3DWithFeatureBase<TTriggeringObject> : Trigger3DFeatureBase
+    public abstract class Trigger2DByFeatureBase<TTriggeringObject> : Trigger2DFeatureBase
     {
-        protected async UniTask OnTriggerEnteredBy([NotNull] Collider detectedCollider,
+        protected async UniTask OnTriggerEnteredBy([NotNull] Collider2D detectedCollider,
             [NotNull] TTriggeringObject other) => await Task.CompletedTask;
 
-        protected async UniTask OnTriggerExitedBy([NotNull] Collider detectedCollider,
+        protected async UniTask OnTriggerExitedBy([NotNull] Collider2D detectedCollider,
             [NotNull] TTriggeringObject other) => await Task.CompletedTask;
 
-        protected async UniTask OnTriggerStayedBy([NotNull] Collider detectedCollider,
+        protected async UniTask OnTriggerStayedBy([NotNull] Collider2D detectedCollider,
             [NotNull] TTriggeringObject other) => await Task.CompletedTask;
 
-        protected sealed override async UniTask OnTriggerEnteredBy(Collider detectedCollider, GameObject other)
+        protected sealed override async UniTask OnTriggerEnteredBy(Collider2D detectedCollider, GameObject other)
         {
             if (other.TryGetComponent(out TTriggeringObject triggeringObject))
                 await OnTriggerEnteredBy(detectedCollider, triggeringObject);
         }
 
-        protected sealed override async UniTask OnTriggerExitedBy(Collider detectedCollider, GameObject other)
+        protected sealed override async UniTask OnTriggerExitedBy(Collider2D detectedCollider, GameObject other)
         {
             if (other.TryGetComponent(out TTriggeringObject triggeringObject))
                 await OnTriggerExitedBy(detectedCollider, triggeringObject);
         }
 
-        protected sealed override async UniTask OnTriggerStayedBy(Collider detectedCollider, GameObject other)
+        protected sealed override async UniTask OnTriggerStayedBy(Collider2D detectedCollider, GameObject other)
         {
             if (other.TryGetComponent(out TTriggeringObject triggeringObject))
                 await OnTriggerStayedBy(detectedCollider, triggeringObject);
--- Assets/FastUnityCreationKit/Unity/Features/Physics/Specific/Trigger3DByFeatureBase.cs
+++ Assets/FastUnityCreationKit/Unity/Features/Physics/Specific/Trigger3DByFeatureBase.cs
@@ -7,32 +7,32 @@ using UnityEngine;
 namespace FastUnityCreationKit.Unity.Features.Physics.Specific
 {
     /// <summary>
-    /// Feature base for 2D triggers
+    /// Feature base for 3D triggers
     /// </summary>
-    public abstract class Trigger2DWithFeatureBase<TTriggeringObject> : Trigger2DFeatureBase
+    public abstract class Trigger3DByFeatureBase<TTriggeringObject> : Trigger3DFeatureBase
     {
-        protected async UniTask OnTriggerEnteredBy([NotNull] Collider2D detectedCollider,
+        protected async UniTask OnTriggerEnteredBy([NotNull] Collider detectedCollider,
             [NotNull] TTriggeringObject other) => await Task.CompletedTask;
 
-        protected async UniTask OnTriggerExitedBy([NotNull] Collider2D detectedCollider,
+        protected async UniTask OnTriggerExitedBy([NotNull] Collider detectedCollider,
             [NotNull] TTriggeringObject other) => await Task.CompletedTask;
 
-        protected async UniTask OnTriggerStayedBy([NotNull] Collider2D detectedCollider,
+        protected async UniTask OnTriggerStayedBy([NotNull] Collider detectedCollider,
             [NotNull] TTriggeringObject other) => await Task.CompletedTask;
 
-        protected sealed override async UniTask OnTriggerEnteredBy(Collider2D detectedCollider, GameObject other)
+        protected sealed override async UniTask OnTriggerEnteredBy(Collider detectedCollider, GameObject other)
         {
             if (other.TryGetComponent(out TTriggeringObject triggeringObject))
                 await OnTriggerEnteredBy(detectedCollider, triggeringObject);
         }
 
-        protected sealed override async UniTask OnTriggerExitedBy(Collider2D detectedCollider, GameObject other)
+        protected sealed override async UniTask OnTriggerExitedBy(Collider detectedCollider, GameObject other)
         {
             if (other.TryGetComponent(out TTriggeringObject triggeringObject))
                 await OnTriggerExitedBy(detectedCollider, triggeringObject);
         }
 
-        protected sealed override async UniTask OnTriggerStayedBy(Collider2D detectedCollider, GameObject other)
+        protected sealed override async UniTask OnTriggerStayedBy(Collider detectedCollider, GameObject other)
         {
             if (other.TryGetComponent(out TTriggeringObject triggeringObject))
                 await OnTriggerStayedBy(detectedCollider, triggeringObject);
