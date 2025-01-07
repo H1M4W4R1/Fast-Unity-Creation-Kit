2dd57c6d2fe1d16c9adc1cfc4e23c4ebf6e94c01
Patryk Pastuszka <nov11157@gmail.com>
2025-01-07 07:04:59 +0100

    RuntimeDatabase.cs as non-unique containers support.

--- Assets/FastUnityCreationKit/Data/Containers/RuntimeDatabase.cs
+++ Assets/FastUnityCreationKit/Data/Containers/RuntimeDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using FastUnityCreationKit.Data.Abstract;
+using FastUnityCreationKit.Data.Interfaces;
 using FastUnityCreationKit.Saving.Interfaces;
 using FastUnityCreationKit.Saving.Utility;
 using FastUnityCreationKit.Structure.Singleton;
@@ -37,15 +38,46 @@ namespace FastUnityCreationKit.Data.Containers
         /// </summary>
         public static void RegisterData([NotNull] TDataType data)
         {
-            if (!Instance.Contains(data)) Instance.Add(data);
+            // Some instances may implement IUniqueDataContainer, so we need to check if the data is already there.
+            if (Instance is not IUniqueDataContainer || !Instance.Contains(data))
+            {
+                Instance.Add(data);
+            }
         }
 
         /// <summary>
-        ///     Remove data from the database.
+        ///     Remove data from the database (first found instance).
         /// </summary>
         public static void UnregisterData([NotNull] TDataType data)
         {
             Instance.Remove(data);
+        }
+        
+        /// <summary>
+        ///     Unregister all data that is same as provided one from the database.
+        /// </summary>
+        public static void UnregisterAllData([NotNull] TDataType data)
+        {
+            // Handle the case when the instance is IUniqueDataContainer - only one data instance can be stored,
+            // so we can just remove it.
+            if (Instance is IUniqueDataContainer)
+            {
+                Instance.Remove(data);
+            }
+            else
+            {
+                // Handle the case when the instance is not IUniqueDataContainer - multiple data instances can be stored,
+                // so we need to iterate through all data instances and remove them.
+                for (int i = 0; i < Instance.Count; i++)
+                {
+                    // Check if the data is the same as the one we want to remove.
+                    // We check against data as Instance[i] may be null, this way we don't need to check for null.
+                    if(!data.Equals(Instance[i])) continue;
+                    
+                    // Unregister the data.
+                    Instance.Remove(data);
+                }
+            }
         }
 
         ~RuntimeDatabase()
